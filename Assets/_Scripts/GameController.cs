5ad3ac002b026e3b95812ba162a4fd761e328044
Philip <hyngsuki@gmail.com>
2019-10-19 16:45:45 -0400

    Philip
    Test 1 project

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    //References
+    private GameController gC;
+
+    [Header("Scene Game Objects")]
+    public GameObject cloud;
+    public GameObject island;
+    public int numberOfClouds;
+    public List<GameObject> clouds;
+
+    [Header("Audio Sources")]
+    public SoundClip activeSoundClip;
+    public AudioSource[] audioSources;
+
+    [Header("Scoreboard")]
+    [SerializeField]
+    private int _lives;
+
+    [SerializeField]
+    private int _score;
+
+    public Text livesLabel;
+    public Text scoreLabel;
+    public Text highScoreLabel;
+
+    public GameObject highScore;
+
+    [Header("UI Control")]
+    public GameObject startLabel;
+    public GameObject startButton;
+    public GameObject endLabel;
+    public GameObject restartButton;
+
+    // public properties
+    public int Lives
+    {
+        get
+        {
+            return _lives;
+        }
+
+        set
+        {
+            _lives = value;
+            highScore.GetComponent<HighScore>().lives = _lives;
+            if(_lives < 1)
+            {
+                
+                SceneManager.LoadScene("End");
+            }
+            else
+            {
+                livesLabel.text = "Lives: " + _lives.ToString();
+            }
+           
+        }
+    }
+
+    public int Score
+    {
+        get
+        {
+            return _score;
+        }
+
+        set
+        {
+            _score = value;
+
+            highScore.GetComponent<HighScore>().score = _score;
+
+            if (highScore.GetComponent<HighScore>().highScore < _score)
+            {
+                highScore.GetComponent<HighScore>().highScore = _score;
+            }
+            scoreLabel.text = "Score: " + _score.ToString();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObjectInitialization();
+        SceneConfiguration();
+        GameObject gCO = GameObject.FindWithTag("GameController");
+        gC = gCO.GetComponent<GameController>();
+    }
+
+    private void GameObjectInitialization()
+    {
+        highScore = GameObject.Find("HighScore");
+
+        startLabel = GameObject.Find("StartLabel");
+        endLabel = GameObject.Find("EndLabel");
+        startButton = GameObject.Find("StartButton");
+        restartButton = GameObject.Find("RestartButton");
+    }
+
+
+    private void SceneConfiguration()
+    {
+
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "Start":
+                scoreLabel.enabled = false;
+                livesLabel.enabled = false;
+                highScoreLabel.enabled = false;
+                startButton.SetActive(true);
+                endLabel.SetActive(false);
+                restartButton.SetActive(false);
+                activeSoundClip = SoundClip.NONE;
+                break;
+            case "Main":
+                highScoreLabel.enabled = false;
+                startLabel.SetActive(false);
+                startButton.SetActive(false);
+                endLabel.SetActive(false);
+                restartButton.SetActive(false);
+                activeSoundClip = SoundClip.ENGINE;
+                Lives = 5;
+                Score = 0;
+                break;
+
+            case "Level2":
+                highScoreLabel.enabled = false;
+                startLabel.SetActive(false);
+                startButton.SetActive(false);
+                endLabel.SetActive(false);
+                restartButton.SetActive(false);
+                activeSoundClip = SoundClip.ENGINE;
+                Lives = highScore.GetComponent<HighScore>().lives;
+                Score = highScore.GetComponent<HighScore>().score;
+                break;
+            case "End":
+                scoreLabel.enabled = false;
+                livesLabel.enabled = false;
+                startLabel.SetActive(false);
+                startButton.SetActive(false);
+                activeSoundClip = SoundClip.NONE;
+                highScoreLabel.text = "High Score: " + highScore.GetComponent<HighScore>().highScore;
+                break;
+        }
+
+        if ((activeSoundClip != SoundClip.NONE) && (activeSoundClip != SoundClip.NUM_OF_CLIPS))
+        {
+            AudioSource activeAudioSource = audioSources[(int)activeSoundClip];
+            activeAudioSource.playOnAwake = true;
+            activeAudioSource.loop = true;
+            activeAudioSource.volume = 0.5f;
+            activeAudioSource.Play();
+        }
+
+
+
+        // creates an empty container (list) of type GameObject
+        clouds = new List<GameObject>();
+
+        for (int cloudNum = 0; cloudNum < numberOfClouds; cloudNum++)
+        {
+            clouds.Add(Instantiate(cloud));
+        }
+
+        Instantiate(island);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreLabel.text = "Score: " + _score.ToString();
+        livesLabel.text = "Lives: " + _lives.ToString();
+        if(_score >= 500 && SceneManager.GetActiveScene().name == "Main")
+        {
+            nextLevel();
+        }
+    }
+
+    // Event Handlers
+    public void OnStartButtonClick()
+    {
+        DontDestroyOnLoad(highScore);
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OnRestartButtonClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    
+    void nextLevel()
+    {
+        DontDestroyOnLoad(highScore);
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Assets/_Scripts/PlayerControllerHoriz.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class PlayerControllerHoriz : MonoBehaviour
+{
+    public Speed speed;
+    public Boundary boundary;
+
+    public GameController gameController;
+
+    // private instance variables
+    private AudioSource _thunderSound;
+    private AudioSource _yaySound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _thunderSound = gameController.audioSources[(int)SoundClip.THUNDER];
+        _yaySound = gameController.audioSources[(int)SoundClip.YAY];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    public void Move()
+    {
+        Vector2 newPosition = transform.position;
+
+        if(Input.GetAxis("Vertical") > 0.0f)
+        {
+            newPosition += new Vector2(0.0f,speed.max);
+        }
+
+        if (Input.GetAxis("Vertical") < 0.0f)
+        {
+            newPosition += new Vector2(0.0f, speed.min);
+        }
+
+        transform.position = newPosition;
+    }
+
+    public void CheckBounds()
+    {
+        // check Top boundary
+        if(transform.position.y > boundary.Top)
+        {
+            transform.position = new Vector2(transform.position.x, boundary.Top);
+        }
+
+        // check Bottom boundary
+        if (transform.position.y < boundary.Bottom)
+        {
+            transform.position = new Vector2(transform.position.x,boundary.Bottom);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Cloud":
+                _thunderSound.Play();
+                gameController.Lives -= 1;
+                break;
+            case "Island":
+                _yaySound.Play();
+                gameController.Score += 100;
+                break;
+        }
+    }
+
+}
